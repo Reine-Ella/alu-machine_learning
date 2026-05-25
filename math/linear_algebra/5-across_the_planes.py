609266acb4f397bac979578478ad6d9e4bc8a1ba
Reine-Ella <r.dusenayo@alustudent.com>
2026-05-25 11:38:39 -0700

    Linear Algebra

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+def add_matrices2D(mat1, mat2):
+    if len(mat1) != len(mat2):
+        return None
+    if any(len(mat1[i]) != len(mat2[i]) for i in range(len(mat1))):
+        return None
+    return [[mat1[i][j] + mat2[i][j] for j in range(len(mat1[i]))] for i in range(len(mat1))]
